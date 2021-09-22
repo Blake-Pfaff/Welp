32e17c152a034b7cb740bc616b004e0e3868602a
Blake Pfaff <blake.pfaff@sgkinc.com>
2021-09-22 12:26:24 -0500

    builds out API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { API } from "./services/api.js";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [businesses, setBusinesses] = useState({});
+
+  const getBusinesses = async (searchTerm) => {
+    const data = await API.getBusinesses(searchTerm);
+    setBusinesses(data);
+    console.log(businesses);
+  };
+
+  return (
+    <div>
+      <div>App</div>
+      <button type="button" onClick={() => getBusinesses("pizza")}>
+        Get Pizza Businesses
+      </button>
+      <pre>{JSON.stringify(businesses, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+const BASE_URL =
+  "https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search";
+const BASE_CITY = "Naperville";
+
+export const API = {
+  getBusinesses: async (searchTerm) => {
+    const url = new URL(BASE_URL);
+    url.searchParams.append("location", BASE_CITY);
+    url.searchParams.append("categories", searchTerm);
+
+    const apiKey = `Bearer ${process.env.REACT_APP_YELP_API_KEY}`;
+
+    const req = await fetch(url.href, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+    const payload = await req.json();
+
+    return payload;
+  },
+};
+
+export default API;
